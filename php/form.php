be2046bbaddd1295e7a24a697643e97a6a0e09c9
Luciano Bertolano <lucianobertolano@gmail.com>
2022-10-28 21:05:02 -0300

    ajustes

---
+++ php/form.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Conectamos el form con el server  */
+
+$nombre = $_POST['name'];
+$mail = $_POST['email'];
+$mensaje = $_POST['textarea'];
+
+/* como nos llega el cuerpo del mail */
+
+$mensaje
